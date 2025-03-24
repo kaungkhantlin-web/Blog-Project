ee2ca6971ce89d0b5ef71fe451aff15fc680ac19
kaungkhantlin-web <jefferson13.epiphany@gmail.com>
2025-03-24 21:55:11 +0700

    change the recipient email address.

--- main.py
+++ main.py
@@ -298,7 +298,7 @@ def send_email(name, email, phone, message):
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MAIL_ADDRESS, MAIL_APP_PW)
-        connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
+        connection.sendmail(from_addr=MAIL_ADDRESS, to_addrs=MAIL_ADDRESS, msg=email_message)
 
 
 if __name__ == "__main__":
